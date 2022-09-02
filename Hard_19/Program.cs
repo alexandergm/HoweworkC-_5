f998044913d138746fe16fa1c95a7c653c0e84f8
alexandergm <sasha_261@outlook.com>
2022-09-03 00:42:47 +0300

    Complete Hard_19

---
+++ Hard_19/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число: ");
+long number = Convert.ToInt64(Console.ReadLine());
+long result = 0;
+result = RevertNum(number);
+Palynodrome();
+
+long RevertNum(long count)
+{
+    while (count > 0) {
+        result *= 10; 
+        result += count % 10; 
+        count /= 10; 
+    }
+    return result;
+}
+void Palynodrome()
+{
+    if (number == result)
+    {
+        Console.WriteLine("Число " + number + " является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число " + number + " не является палиндромом");
+    }
+}
