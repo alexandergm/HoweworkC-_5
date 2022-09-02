6f7ca33ea1b525f86a12678e5d4a3492e2c9b5af
alexandergm <sasha_261@outlook.com>
2022-09-03 00:33:38 +0300

    Complete Task_38

---
+++ Task_38/Program.cs
@@ -0,0 +1,26 @@
+﻿double[] array = new double[10];
+int max = 0;
+int min = 0;
+double diff = MinMax(InitialMas());
+Console.WriteLine(diff);
+
+double[] InitialMas()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round((new Random().Next(0, 10) + new Random().NextDouble()),2);
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+    return array;
+}
+
+double MinMax(double[] mas)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[max] < array[i]) max = i;
+        else if(array[min] > array[i]) min = i; 
+    }
+    return Math.Round((array[max] - array[min]),2);
+}
