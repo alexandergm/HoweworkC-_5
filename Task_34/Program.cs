b0460240711d8910d6f511b221d5f653e830c36f
alexandergm <sasha_261@outlook.com>
2022-09-03 00:06:30 +0300

    Complete Task_34

--- Task_34/Program.cs
+++ Task_34/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] arr = new int[10];
+int sum = 0;
+Console.WriteLine(Even(InitialMas()));
+
+int[] InitialMas()
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+    return arr;
+}
+
+int Even(int[] mas)
+{
+    foreach(int count in arr)
+    {
+        if(count % 2 == 0) sum++; 
+    }
+    return sum;
+}
