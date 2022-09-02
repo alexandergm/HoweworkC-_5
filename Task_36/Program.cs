9d64ca75a0a3a6d7af6ee65835b57fc1e9fcdd87
alexandergm <sasha_261@outlook.com>
2022-09-03 00:12:47 +0300

    Complete Task_36

---
+++ Task_36/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] arr = new int[10];
+int sum = 0;
+Console.WriteLine(OddSum(InitialMas()));
+
+int[] InitialMas()
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 100);
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+    return arr;
+}
+int OddSum(int[] mas)
+{
+    for(int i = 1; i < arr.Length; i += 2)
+    {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
